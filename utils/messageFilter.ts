4fd1ec21cc36375a4eebf1ce4a054b41a0c7a044
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2024-12-20 14:04:16 +0530

    Delete utils directory

--- utils/messageFilter.ts
+++
@@ -1,9 +0,0 @@
-import { PHONE_PATTERN, EMAIL_PATTERN } from '../constants/patterns';
-
-export function filterMessage(message: string): string {
-  if (PHONE_PATTERN.test(message) || EMAIL_PATTERN.test(message)) {
-    return "[Message Filtered: Contact information detected]";
-  }
-  return message;
-}
-
